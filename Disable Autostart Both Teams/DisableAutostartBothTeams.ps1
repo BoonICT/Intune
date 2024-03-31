0a4d30ebd1e6e6d95451f51d5f04c8e0650144a4
BoonICT <82510474+BoonICT@users.noreply.github.com>
2024-03-31 17:02:32 +0200

    Combined script for Teams Classic & Teams New

---
+++ Disable Autostart Both Teams/DisableAutostartBothTeams.ps1
@@ -0,0 +1,46 @@
+﻿<#
+Author:  Ed Boon
+Script:  DisableAutostartBothTeams.ps1
+Description: Prevent Microsoft Classic and NEW Teams to start automatically.
+The script must run in USER context
+
+Release notes:
+Version 1.0: 2024-02-22 - Original published version.
+
+The script is provided "AS IS" with no warranties.
+#>
+
+Start-Transcript -Path "$env:TEMP\DisableAutostartBothTeams.log" | Out-Null
+
+# Remove Run registry entry to disable auto-start of Classic Teams
+$runKeyPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+$runPropertyName = "com.squirrel.Teams.Teams"
+
+$runProperty = Get-ItemProperty -Path $runKeyPath -Name $runPropertyName -ErrorAction SilentlyContinue
+if ($runProperty -ne $null) {
+    Remove-ItemProperty -Path $runKeyPath -Name $runPropertyName -ErrorAction SilentlyContinue
+    Write-Host "Autostart of Classic Teams has been Disabled"
+} else {
+    Write-Host "Autostart Classic Teams was already diasabled or not there at all"
+}
+
+# Modify State Key to disable New Teams
+$hpath = "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\MSTeams_8wekyb3d8bbwe\TeamsTfwStartupTask"
+
+if (Test-Path $hpath) {
+    # Modify State
+    Set-ItemProperty -Path $hpath -Name "State" -Value "0"
+
+    # Modify LastDisabledTime
+    $epoch = (Get-Date -Date ((Get-Date).DateTime) -UFormat %s)
+    Set-ItemProperty -Path $hpath -Name "LastDisabledTime" -Value $epoch
+
+    Write-Host "Autostart NEW Teams has been Disabled"
+} else {
+    Write-Host "NEW Teams is not found"
+}
+
+Write-Host "Disable Autostart of Teams configuration completed."
+
+
+Stop-Transcript -Verbose
